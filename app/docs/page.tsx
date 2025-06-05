789a686bd8faa051cd4fde0a7b4186b94b494bf4
Evan Hallmark <ehallmar@usc.edu>
2025-06-04 21:13:05 -0700

    docs test

--- app/docs/page.tsx
+++ app/docs/page.tsx
@@ -2,14 +2,14 @@
 
 export default function DocsPage() {
   return (
-      <div className="h-[calc(100vh-4rem)]">
-        <iframe
-          src="/docs/index.html"
-          className="w-full h-full"
-          title="API Documentation"
-          frameBorder="0"
-          allowFullScreen
-        />
-      </div>
+    <div className="h-[calc(100vh-4rem)] w-full">
+      <iframe
+        src="/docs/index.html"
+        className="w-full h-full"
+        title="API Documentation"
+        frameBorder="0"
+        allowFullScreen
+      />
+    </div>
   );
 }
