d0a79a2f291be60ad54d1189bdf5c84b1dab393a
Evan Hallmark <ehallmar@usc.edu>
2025-06-05 16:16:09 -0700

    minor styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -80,7 +80,7 @@ export default function Navbar() {
                   href={item.href}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-800 dark:text-gray-300 hover:text-foreground hover:bg-gray-800 flex items-center justify-between"
+                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-800 dark:text-gray-300 hover:text-foreground hover:bg-gray-100 dark:hover:bg-gray-800 flex items-center justify-between"
                 >
                   <span className="flex items-center">
                     {item.name} {item.external && <ArrowTopRightOnSquareIcon className="ml-1 h-4 w-4" />}
@@ -90,7 +90,7 @@ export default function Navbar() {
                 <Link
                   key={item.name}
                   href={item.href}
-                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-800 dark:text-gray-300 hover:text-foreground hover:bg-gray-800 flex items-center justify-between"
+                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-800 dark:text-gray-300 hover:text-foreground hover:bg-gray-100 dark:hover:bg-gray-800 flex items-center justify-between"
                 >
                   <span className="flex items-center">
                     {item.name} {item.external && <ArrowTopRightOnSquareIcon className="ml-1 h-4 w-4" />}
